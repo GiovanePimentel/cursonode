728668b4f86cb309c594734c91bebce50c860be1
Giovane Pimentel <giovane.rcgp@gmail.com>
2022-04-11 20:37:19 -0300

    crypto.pbfk2 teste de execucao multi thread e single thread

--- test.js
+++ test.js
@@ -12,3 +12,15 @@ logHashTime();
 logHashTime();
 logHashTime();
 
+function logHashTimeSync(){
+        return crypto.pbkdf2Sync("a","b",100000,512,"sha512")
+}
+const start2 = Date.now();
+
+let sync1 = logHashTimeSync();
+console.log(`Sync: ${Date.now() - start2}`)
+let sync2 = logHashTimeSync();
+console.log(`Sync: ${Date.now() - start2}`)
+
+console.log(sync1);
+console.log(sync2);
